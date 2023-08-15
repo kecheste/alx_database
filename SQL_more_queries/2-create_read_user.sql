5b1b1df660cc6561fc6d7005bffd990087430cf9
kecheste <aben.ezer443@gmail.com>
2023-08-15 19:23:49 +0300

    create read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ USE hbtn_0d_2;
 
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON *.* TO user_0d_2@localhost;
